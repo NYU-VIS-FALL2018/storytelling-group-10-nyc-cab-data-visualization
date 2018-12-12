5147f1c247303ec09b2af21ed8fc4fe7bbb50e66
Pranav Reddy Mandala <panny.pranav@gmail.com>
2018-12-12 15:54:12 -0500

    UI FIxes

--- js/heatmap.js
+++ js/heatmap.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
     days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
     // days = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-    times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+    times = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
     datasets = ["dataset/final_project_data.csv"];
 
 var svg = d3.select("#chart").append("svg")
@@ -26,6 +26,7 @@ var dayLabels = svg.selectAll(".dayLabel")
     .attr("x", 0)
     .attr("y", function (d, i) { return i * gridSize; })
     .style("text-anchor", "end")
+    .style("fill", "white")
     .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
     .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
@@ -35,6 +36,7 @@ var timeLabels = svg.selectAll(".timeLabel")
     .text(function(d) { return d; })
     .attr("x", function(d, i) { return i * gridSize; })
     .attr("y", 0)
+    .attr("fill", "white")
     .style("text-anchor", "middle")
     .attr("transform", "translate(" + gridSize / 2 + ", -6)")
     .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
@@ -52,9 +54,9 @@ function(error, data) {
     // var colorScale = d3.scaleQuantile()
     //     .domain([0, buckets - 1, d3.max(data, function (d) { return Math.log2(d.value); })])
     //     .range(colors);
-    var colorScale = d3.scaleLinear().domain([1,d3.max(data, function (d) { return Math.log2(d.value); })])
-      .interpolate(d3.interpolateHcl)
-      .range([d3.rgb("#ffffd9"), d3.rgb("#c7e9b4"), d3.rgb("#41b6c4"), d3.rgb('#225ea8')]);
+    var max = d3.max(data, function (d) { return Math.log2(d.value); });
+    var colorScale = d3.scaleLinear().domain([max, max / 2, max/3, 1])
+    .range([d3.rgb("#FFFF00"),d3.rgb("#FFFF33"), d3.rgb("#FFFF66"), d3.rgb("#FFFF99")]);
 
     var cards = svg.selectAll(".hour")
         .data(data, function(d) { return ((d.day%7)+1)+':'+d.hour;});
@@ -62,7 +64,7 @@ function(error, data) {
     cards.append("title");
 
     cards.enter().append("rect")
-        .attr("x", function(d) { return (d.hour - 1) * gridSize; })
+        .attr("x", function(d) { return (d.hour - 1) * gridSize + 45; })
         .attr("y", function(d) { return (((d.day%7)+1) - 1) * gridSize; })
         .attr("rx", 4)
         .attr("ry", 4)
